5a1559d8057626358ab66dee5fe08d70e1fa76bb
RincewindWizzard <git@magierdinge.de>
2019-08-22 13:06:59 +0200

    default ist nicht mehr clean build

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--dst", help="Path to destination of build.")
     parser.add_argument("--cache", help="Path to image cache.")
+    parser.add_argument("--clean", help="Remove previous build artifacts")
     args = parser.parse_args()
 
     if args.cache:
@@ -16,7 +17,9 @@ def main():
     if args.dst:
         settings.BUILD_PATH = os.path.abspath(args.dst)
 
-    builder.clean()
+    if args.clean:
+        builder.clean()
+
     builder.build()
 
 if __name__ == '__main__':
